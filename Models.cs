1b53b575a0d583a33d0dfe603a104a87acad83f8
paufer2337 <paufer2337@gmail.com>
2024-06-18 23:35:15 +0200

    Uploading EF assignment

---
+++ Models.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+public class DB : DbContext
+{
+    public DbSet<Blog> Blogs { get; set; }
+    public DbSet<Post> Posts { get; set; }
+    public DbSet<User> Users { get; set; }
+
+    public string DbPath { get; }
+
+    public DB()
+    {
+        var folder = @"C:\Users\pauli\source\repos\EFBlogging";
+
+        DbPath = Path.Combine(folder, "efblogging.db");
+
+        Directory.CreateDirectory(folder);
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder options)
+        => options.UseSqlite($"Data Source={DbPath}");
+}
+
+public class Blog
+{
+    public int BlogId { get; set; }
+    public string? Url { get; set; }
+    public string? Name { get; set; }
+    public List<Post> Posts { get; } = new List<Post>();
+}
+
+public class Post
+{
+    public int PostId { get; set; }
+    public string? Title { get; set; }
+    public string? Content { get; set; }
+    public DateTime PublishedOn { get; set; }
+    public int BlogId { get; set; }
+    public Blog? Blog { get; set; }
+    public int? UserId { get; set; }
+    public User? User { get; set; }
+}
+
+public class User
+{
+    public int UserId { get; set; }
+    public string? Username { get; set; }
+    public string? Password { get; set; }
+    public List<Post> Posts { get; } = new List<Post>();
+}
---
+++ Program.cs
@@ -0,0 +1,111 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+public class Program
+{
+    public void Run()
+    {
+
+        using (var db = new DB())
+        {
+            Console.WriteLine($"SQLite DB Located at: {db.DbPath}");
+            ResetDatabase(db);
+            ImportCsv(db, "users.csv", "posts.csv", "blogs.csv");
+            DisplayTree(db);
+        }
+    }
+    public void ResetDatabase(DB db)
+    {
+        db.Database.ExecuteSqlRaw("DELETE FROM Posts");
+        db.Database.ExecuteSqlRaw("DELETE FROM Blogs");
+        db.Database.ExecuteSqlRaw("DELETE FROM Users");
+    }
+
+    public void ImportCsv(DB db, params string[] csvFiles)
+    {
+        foreach (var csvFile in csvFiles)
+        {
+            string[] lines = File.ReadAllLines(csvFile);
+
+            if (csvFile.Contains("user"))
+            {
+                foreach (var line in lines.Skip(1))
+                {
+                    var data = line.Split(',');
+                    var user = new User
+                    {
+                        UserId = int.Parse(data[0]),
+                        Username = data[1],
+                        Password = data[2],
+                    };
+
+                    db.Users.Add(user);
+                }
+            }
+            else if (csvFile.Contains("post"))
+            {
+                foreach (var line in lines.Skip(1))
+                {
+                    var data = line.Split(',');
+                    var post = new Post
+                    {
+                        PostId = int.Parse(data[0]),
+                        Title = data[1],
+                        Content = data[2],
+                        PublishedOn = DateTime.Parse(data[3]),
+                        BlogId = int.Parse(data[4]),
+                        UserId = int.Parse(data[5])
+                    };
+
+                    db.Posts.Add(post);
+                }
+            }
+            else if (csvFile.Contains("blog"))
+            {
+                foreach (var line in lines.Skip(1))
+                {
+                    var data = line.Split(',');
+                    var blog = new Blog
+                    {
+                        BlogId = int.Parse(data[0]),
+                        Url = data[1],
+                        Name = data[2],
+                    };
+
+                    db.Blogs.Add(blog);
+                }
+            }
+        }
+
+        db.SaveChanges();
+    }
+
+    public void DisplayTree(DB db)
+    {
+        var users = db.Users
+            .Include(u => u.Posts)
+                .ThenInclude(p => p.Blog)
+            .ToList();
+
+        foreach (var user in users)
+        {
+            Console.WriteLine($"User: {user.Username}");
+
+            foreach (var post in user.Posts)
+            {
+                Console.WriteLine($"  Post: {post.Title}");
+
+                if (post.Blog != null)
+                    Console.WriteLine($"    Blog: {post.Blog.Name}");
+            }
+        }
+    }
+
+    public static void Main()
+    {
+        new Program().Run();
+    }
+}
